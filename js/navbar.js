7c5c6cf89550a1b73d7369a91dadef94032bd625
tompsognathus <tompsognathus@gmail.com>
2025-01-18 04:00:16 +0100

    responsive navbar

---
+++ js/navbar.js
@@ -0,0 +1,23 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function handleResponsiveNavbar() {
+  var x = document.getElementById("header-navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+// Create a condition that targets viewports at least 992px wide
+const mediaQuery = window.matchMedia('(min-width: 992px)');
+
+function handleTabletChange(e) {
+  if (e.matches) {
+    // Remove responsive class from navbar to switch from mobile/tablet to pc view
+    var x = document.getElementById("header-navbar");
+    x.className = "navbar";
+  }
+}
+
+// Register event listener
+mediaQuery.addListener(handleTabletChange);
