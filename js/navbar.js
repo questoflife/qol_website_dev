10d83f7cb35887a008e3525a00108d0c8cedee3b
tompsognathus <tompsognathus@gmail.com>
2025-01-20 13:03:54 +0100

    adding bootstrap

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,10 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function handleResponsiveNavbar() {
-  var x = document.getElementById("header-navbar");
-  if (x.className === "navbar") {
+  var x = document.getElementById("header-mynavbar");
+  if (x.className === "mynavbar") {
     x.className += " responsive";
   } else {
-    x.className = "navbar";
+    x.className = "mynavbar";
   }
 
   var x = document.getElementById("page-header");
@@ -21,9 +21,9 @@ const mediaQuery = window.matchMedia('(min-width: 992px)');
 
 function handleTabletChange(e) {
   if (e.matches) {
-    // Remove responsive class from navbar to switch from mobile/tablet to pc view
-    var x = document.getElementById("header-navbar");
-    x.className = "navbar";
+    // Remove responsive class from mynavbar to switch from mobile/tablet to pc view
+    var x = document.getElementById("header-mynavbar");
+    x.className = "mynavbar";
   }
 }
 
